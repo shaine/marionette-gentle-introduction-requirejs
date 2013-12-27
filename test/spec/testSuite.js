713d2a97a171b8aeb0bb61b6b51ebec9487dc1d7
Shaine Hatch <shaine@digitaltrike.com>
2013-12-27 10:44:26 -0700

    meh

--- test/spec/testSuite.js
+++ test/spec/testSuite.js
@@ -3,7 +3,8 @@ define(function() {
 
     /* return an array of specs to be run */
     return {
-        specs: ['spec/exampleTest'
+        specs: [
+            'spec/exampleTest'
         ]
     };
 });
