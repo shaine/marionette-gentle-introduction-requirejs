808a8e7c3f3cfed94065f6d45c8740b9c30d209f
Shaine Hatch <shaine@digitaltrike.com>
2014-01-06 08:48:18 -0700

    Show the loading view

--- app/scripts/controllers/show/contact.js
+++ app/scripts/controllers/show/contact.js
@@ -2,13 +2,17 @@ define([
     'backbone',
     'communicator',
     'views/show/contact',
-    'views/show/contact-missing'
+    'views/show/contact-missing',
+    'views/loading'
 ],
-function( Backbone, Communicator, ContactShowView, ContactMissingShowView ) {
+function( Backbone, Communicator, ContactShowView, ContactMissingShowView, LoadingView ) {
     'use strict';
 
     return new (Backbone.Marionette.Controller.extend({
         showContact: function(id) {
+            var loadingView = new LoadingView();
+            Communicator.mediator.trigger('app:show', loadingView);
+
             var fetchingContact = Communicator.reqres.request('contact:entity', id);
             $.when(fetchingContact).done(function(contact) {
                 var contactShowView;
--- app/scripts/views/loading.js
+++ app/scripts/views/loading.js
@@ -1,12 +1,38 @@
 define([
-    'backbone'
+    'backbone',
+    'hbs!tmpl/loading'
 ],
-function(Backbone){
+function(Backbone, LoadingViewTmpl){
     'use strict';
 
-    return Backbone.View.extend({
+    return Backbone.Marionette.ItemView.extend({
         initialize: function() {
             console.log('initialize a Loading View');
+        },
+
+        template: LoadingViewTmpl,
+
+
+        onShow: function(){
+            var opts = {
+                lines: 13, // The number of lines to draw
+                length: 20, // The length of each line
+                width: 10, // The line thickness
+                radius: 30, // The radius of the inner circle
+                corners: 1, // Corner roundness (0..1)
+                rotate: 0, // The rotation offset
+                direction: 1, // 1: clockwise, -1: counterclockwise
+                color: '#000', // #rgb or #rrggbb
+                speed: 1, // Rounds per second
+                trail: 60, // Afterglow percentage
+                shadow: false, // Whether to render a shadow
+                hwaccel: false, // Whether to use hardware acceleration
+                className: 'spinner', // The CSS class to assign to the spinner
+                zIndex: 2e9, // The z-index (defaults to 2000000000)
+                top: '30px', // Top position relative to parent in px
+                left: 'auto' // Left position relative to parent in px
+            };
+            $('#spinner').spin(opts);
         }
     });
 });
